e6f79d852b90847bdd925a9356ff487ee7d0d1f4
Mitchell Lin <mtlin@usc.edu>
2022-09-19 13:57:46 -0700

    Bomb functionality complete.

---
+++ app/src/main/java/com/example/gridlayout/Results.java
@@ -0,0 +1,9 @@
+package com.example.gridlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Results extends MainActivity {
+
+    // Get timer value
+    // Give this thing ability to manipulate
+}
